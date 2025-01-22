aad8d0bca87faedcfc8cccbd65af985d6afbdce8
GabryTm047 <gabriele.costa@itsincom.it>
2025-01-22 15:24:53 +0100

    #3 - start setup chat section in gameplanner

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -1,25 +1,26 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { MainLayoutComponent } from './components/layout/main-layout/main-layout.component';
-import { AuthLayoutComponent } from './components/layout/auth-layout/auth-layout.component';
-import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { DashboardAdminComponent } from './components/admin/dashboard-admin/dashboard-admin.component';
-import { UsersAdminComponent } from './components/admin/users-admin/users-admin.component';
+import { CreateEventComponent } from './components/admin/events-admin/create-event/create-event.component';
+import { EventDetailAdminComponent } from './components/admin/events-admin/eventdetail-admin/eventdetail-admin.component';
 import { EventsAdminComponent } from './components/admin/events-admin/events-admin.component';
 import { GamesAdminComponent } from './components/admin/games-admin/games-admin.component';
-import { EventsComponent } from './components/events/events.component';
-import { EventDetailComponent } from './components/event-detail/event-detail.component';
-import { MobileComponent } from './components/mobile/mobile.component';
-import { EventDetailAdminComponent } from './components/admin/events-admin/eventdetail-admin/eventdetail-admin.component';
+import { UsersAdminComponent } from './components/admin/users-admin/users-admin.component';
 import { ConfirmRegistrationComponent } from './components/confirm-registration/confirm-registration.component';
+import { EventDetailComponent } from './components/event-detail/event-detail.component';
+import { EventsComponent } from './components/events/events.component';
+import { HomeComponent } from './components/home/home.component';
+import { AuthLayoutComponent } from './components/layout/auth-layout/auth-layout.component';
+import { MainLayoutComponent } from './components/layout/main-layout/main-layout.component';
+import { LoginComponent } from './components/login/login.component';
+import { MobileComponent } from './components/mobile/mobile.component';
+import { RegisterComponent } from './components/register/register.component';
 import { RemoveRegistrationComponent } from './components/remove-registration/remove-registration.component';
-import { CreateEventComponent } from './components/admin/events-admin/create-event/create-event.component';
-import { AuthGuardService } from './services/guards/auth-guard.service';
-import { AdminGuardService } from './services/guards/admin-guard.service';
-import { loadRemoteModule } from '@angular-architects/native-federation';
 import { SchedulerComponent } from './components/scheduler/scheduler.component';
+import { SessionChatComponent } from './components/session-chat/session-chat.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { AdminGuardService } from './services/guards/admin-guard.service';
+import { AuthGuardService } from './services/guards/auth-guard.service';
 
 export const routes: Routes = [
   {
@@ -28,38 +29,40 @@ export const routes: Routes = [
     children: [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', component: HomeComponent },
-      { path: 'userSettings', component: UserProfileComponent,canActivate: [AuthGuardService]},
-      { path: 'events',component: EventsComponent},
+      { path: 'userSettings', component: UserProfileComponent, canActivate: [AuthGuardService] },
+      { path: 'events', component: EventsComponent },
       { path: 'events/:id', component: EventDetailComponent },
       { path: 'mobile', component: MobileComponent },
-      { path: 'scheduler', component: SchedulerComponent }
+      { path: 'scheduler', component: SchedulerComponent },
+      { path: 'chat', component: SessionChatComponent }
     ]
-    },
-    {
+  },
+  {
     path: '',
     component: AuthLayoutComponent,
     children: [
       { path: 'login', component: LoginComponent },
       { path: 'register', component: RegisterComponent },
-      { path: 'dashboard-admin', component: DashboardAdminComponent,canActivate: [AdminGuardService],
-      children:[
-        {path:'users', component:UsersAdminComponent},
-        {path:'events', component:EventsAdminComponent},
-        {path: 'events/create', component: CreateEventComponent },
-        {path: 'events/:id', component: EventDetailAdminComponent },
-        {path:'games', component:GamesAdminComponent}
-      ]
+      {
+        path: 'dashboard-admin', component: DashboardAdminComponent, canActivate: [AdminGuardService],
+        children: [
+          { path: 'users', component: UsersAdminComponent },
+          { path: 'events', component: EventsAdminComponent },
+          { path: 'events/create', component: CreateEventComponent },
+          { path: 'events/:id', component: EventDetailAdminComponent },
+          { path: 'games', component: GamesAdminComponent }
+        ]
       },
-      {path: 'confirm', component: ConfirmRegistrationComponent},
-      {path: 'delete', component: RemoveRegistrationComponent}
+      { path: 'confirm', component: ConfirmRegistrationComponent },
+      { path: 'delete', component: RemoveRegistrationComponent }
     ]
-    },
-    { path: '**', redirectTo: 'home' },
+  },
+  { path: '**', redirectTo: 'home' },
 
-    {
-      path: 'subscription',
-      loadComponent: () => 
-          loadRemoteModule('mfe1', './Component').then((m) => m.AppComponent)
-    }
+  {
+    path: 'subscription',
+    loadComponent: () =>
+      loadRemoteModule('mfe1', './Component').then((m) => m.AppComponent)
+  }
 
 ];
---
+++ projects/shell/src/app/components/session-chat/session-chat.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-session-chat',
+  standalone: true,
+  imports: [],
+  templateUrl: './session-chat.component.html',
+  styleUrl: './session-chat.component.scss'
+})
+export class SessionChatComponent {
+
+}
